3174427bc1c740e22db5d9eb15e0680dff017375
Kenta Waibel <kenta.waibel@gmail.com>
2023-05-04 09:35:52 +0200

    transfer: transfering this repo to the bbw organisation

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Das sind meine Personalien:");
+        System.out.print("Ich bin am:");
+        System.out.print(" 23.05.2006");
+        System.out.print(" geboren.");
+        System.out.println(" ");
+        System.out.print("Ich wohne in: ");
+        System.out.print(8053);
+        System.out.print(" Zürich.");
+        System.out.println(" ");
+        System.out.println("Mein Name ist:");
+        System.out.print("Kenta Waibel");
+    }
+}
